f3ff9c9f755863f9de0d80f9498c03fda1e4a05f
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-10-26 18:27:22 +0200

    Update to Nova 0.12

--- src/main/kotlin/xyz/xenondevs/nova/jetpacks/ui/JetpackOverlay.kt
+++ src/main/kotlin/xyz/xenondevs/nova/jetpacks/ui/JetpackOverlay.kt
@@ -7,21 +7,18 @@ import xyz.xenondevs.nova.ui.overlay.character.MoveCharacters
 
 class JetpackOverlay : ActionbarOverlay {
     
-    override var text: Array<BaseComponent> = getCurrentText()
+    override var components: Array<BaseComponent> = getCurrentComponents()
         private set
-    
-    // 95 is the moved distance, 23 is texture size + 1
-    override val width = 95 + 23
     
     var percentage: Double = 0.0
         set(value) {
             require(value in 0.0..1.0)
             if (field == value) return
             field = value
-            text = getCurrentText()
+            components = getCurrentComponents()
         }
     
-    private fun getCurrentText(): Array<BaseComponent> {
+    private fun getCurrentComponents(): Array<BaseComponent> {
         val stage = (percentage * 38).toInt()
         
         return ComponentBuilder()
